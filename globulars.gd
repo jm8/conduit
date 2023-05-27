6038da14f48b194862d5ce60fc6865fbb4b2d5d4
Christopher <christophermcd0903@gmail.com>
2023-05-27 12:54:58 -0400

    fix globulars.gd

--- globulars.gd
+++ globulars.gd
@@ -1,10 +1,7 @@
 extends Node
 
 var enet_peer := ENetMultiplayerPeer.new()
-<<<<<<< HEAD
 
 var conduit_positions: Array[Vector3] = []
 
 var character: Character
-=======
->>>>>>> 0f052fb (minimap)
