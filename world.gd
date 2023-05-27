48a879e925bec711fd22bcce96d5a813899daf33
Josh <jm8hjkl@gmail.com>
2023-05-27 15:45:49 -0400

    dedicated server part one

--- world.gd
+++ world.gd
@@ -3,21 +3,22 @@ extends Node3D
 const World = preload("res://world.tscn")
 const CharacterScene = preload("res://character.tscn")
 
-@onready var menu = $CanvasLayer
+@onready var menu = $MainMenu
+@onready var host_ui = $HostUi
 @onready var address_entry = %AddressEdit
 @onready var port_entry = %PortEdit
 
 const PORT = 45865
 
 func _on_host_button_pressed():
-	Globulars.enet_peer.create_server(PORT if address_entry.text == "" else int(port_entry.text))
+	var port = PORT if address_entry.text == "" else port_entry.text.to_int()
+	Globulars.enet_peer.create_server(port)
 	multiplayer.multiplayer_peer = Globulars.enet_peer
 	print("Hello")
 	multiplayer.peer_connected.connect(add_player)
 	multiplayer.peer_disconnected.connect(remove_player)
-	add_player(multiplayer.get_unique_id())
-
 	menu.queue_free()
+	host_ui.visible = true
 	
 func _on_join_button_pressed():
 	var address = "localhost" if address_entry.text == "" else address_entry.text
@@ -29,6 +30,9 @@ func _on_join_button_pressed():
 	menu.queue_free()
 
 func add_player(peer_id):
+	print(peer_id)
+	if peer_id == 1:
+		return
 	var character = CharacterScene.instantiate()
 	character.name = str(peer_id)
 	add_child(character)
