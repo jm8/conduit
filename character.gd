62d460d9eb0c80b2dabd69826d1f23e7aa78029b
Josh <jm8hjkl@gmail.com>
2023-05-27 22:54:12 -0400

    turning animations

--- character.gd
+++ character.gd
@@ -8,8 +8,10 @@ class_name Character extends CharacterBody3D
 @onready var body = $Armature/Skeleton3D/Body
 @onready var gun3rd = %Gun3rd
 @onready var gun1st = %Gun1st
+@onready var hand = %Hand
 
 var target_recoil = Vector3()
+var target_hand_rotation = Vector3()
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
@@ -33,20 +35,19 @@ func _ready():
 
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	camera.current = true
-	pass
 
 func _notification(what):
 	if what == NOTIFICATION_APPLICATION_FOCUS_IN:
-		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED	
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _unhandled_input(event):
 	if not is_multiplayer_authority(): return
 
 	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED and event is InputEventMouseMotion:
+		target_hand_rotation += Vector3(-event.relative.y, -event.relative.x, 0) * .001
 		rotate_y(-event.relative.x * .005)
 		camera_rotation.rotation.x += -event.relative.y * .005
 		print(camera_rotation.rotation.x)
-		print(-PI/2, PI/2)
 		camera_rotation.rotation.x = clamp(-PI/2, camera_rotation.rotation.x, PI/2)
 
 func _process(_delta):
@@ -55,16 +56,18 @@ func _process(_delta):
 	if Input.is_action_just_pressed("pause"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
-	if not is_multiplayer_authority(): return
+	if not is_multiplayer_authority(): return 
+
+	hand.rotation = lerp(hand.rotation,target_hand_rotation, .2)
+	target_hand_rotation = lerp(target_hand_rotation, Vector3(), .2)
 
 	camera_recoil.rotation = lerp(camera_recoil.rotation, target_recoil, .2)
 	target_recoil = lerp(target_recoil, Vector3(), .2)
 
 	if Input.is_action_pressed("fire"):
 		if not gun_animation_player.is_playing():
-			target_recoil += Vector3(.15, randf_range(-0.05, 0.05), 0)
+			target_recoil += Vector3(.15 + randf_range(-0.05, 0.05), randf_range(-0.05, 0.05), 0)
 		gun_animation_player.play("fire")
-
 	else:
 		camera.transform.origin = Vector3()
 
