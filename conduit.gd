bf02d7cba7dc525a3b8b39546ff5c4babc61ac5f
Christopher <christophermcd0903@gmail.com>
2023-05-31 11:45:57 -0400

    conduit fill even more supergeil

--- conduit.gd
+++ conduit.gd
@@ -19,26 +19,44 @@ var green_capture_progress: float = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if green_players > 0 and orange_players > 0:
-		state = ConduitState.Neutral
-	if orange_players > 0 and green_players == 0:
-		orange_capture_progress += delta * orange_players
-		green_capture_progress = 0
-		set_instance_shader_parameter("capture_color", Color(1, 0, 0));
-	if green_players > 0 and orange_players == 0:
-		green_capture_progress += delta * green_players
-		orange_capture_progress = 0
-		set_instance_shader_parameter("capture_color", Color(0, 1, 0));
-	if green_players == 0 and orange_players == 0:
-		orange_capture_progress = 0
-		green_capture_progress = 0
-	
-	if orange_capture_progress > CAPTURE_TARGET:
-		state = ConduitState.Orange
-	if green_capture_progress > CAPTURE_TARGET:
-		state = ConduitState.Green
-
+	if state == ConduitState.Neutral:
+		if orange_players > 0 and green_players == 0:
+			orange_capture_progress += delta * orange_players
+		elif green_players > 0 and orange_players == 0:
+			green_capture_progress += delta * green_players
+		elif orange_players == 0 and green_players == 0:
+			orange_capture_progress = max(0, orange_capture_progress - delta)
+			green_capture_progress = max(0, green_capture_progress - delta)
+		
+		if orange_capture_progress > capture_time:
+			state = ConduitState.Orange
+			orange_capture_progress = capture_time
+		elif green_capture_progress > capture_time: 
+			state = ConduitState.Green
+			green_capture_progress = capture_time
+	elif state == ConduitState.Green:
+		if orange_players > 0 and green_players == 0:
+			green_capture_progress -= delta * orange_players
+		else:
+			green_capture_progress += delta * (1 + green_players)
+		if green_capture_progress < 0:
+			green_capture_progress = 0
+			state = ConduitState.Neutral
+	elif state == ConduitState.Orange:
+		print("orange state")
+		if green_players > 0 and orange_players == 0:
+			orange_capture_progress -= delta * green_players
+		else:
+			orange_capture_progress += delta * (1 + green_players)
+		if orange_capture_progress < 0:
+			orange_capture_progress = 0
+			state = ConduitState.Neutral
+		
 	set_instance_shader_parameter("capture_progress", max(orange_capture_progress, green_capture_progress) / CAPTURE_TARGET)
+	if orange_capture_progress > green_capture_progress:
+		set_instance_shader_parameter("capture_color", Color(1.0, 0.0, 0.0))
+	else:
+		set_instance_shader_parameter("capture_color", Color(0.0, 1.0, 0.0))
 
 func _on_capture_area_body_entered(body):
 	var team = body.get("team")
