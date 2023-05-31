1431cbf45a7c58bcc6e857b0fe5b7c14b43d50ae
Christopher <christophermcd0903@gmail.com>
2023-05-30 21:21:04 -0400

    fix minimap

--- full_map_adieu.gd
+++ full_map_adieu.gd
@@ -1,6 +1,8 @@
 extends Node3D
 
 @onready var conduits = [$"Half Map Adieu/conduit 1", $"Half Map Adieu/conduit 2", $"Half Map Adieu/conduit 3", $"Half Map Adieu/conduit 4", $"central conduit", $"Half Map Adieu2/conduit 4", $"Half Map Adieu2/conduit 3", $"Half Map Adieu2/conduit 2", $"Half Map Adieu2/conduit 1"]
+@onready var conduits_adjacency = [0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8]
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
